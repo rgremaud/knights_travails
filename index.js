49929f0989f018f4ba182323bd10bcc3ad4c0407
rgremaud <rgremau@gmail.com>
2026-02-08 09:31:53 -0500

    Add possible moves function

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ Queue of all possible moves:
 */
 
 export function knightMoves(start, end) {
-    // define possible moves
-    const moves = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]];
-    
     // set a track for all visited notes
     const visited = [];
+
+    // determine all possible moves
+    const posMoves = possibleMoves(start);
 
     // set array for moves made
     const visitedLocations = [];
@@ -45,4 +45,16 @@ export function knightMoves(start, end) {
 
     const answer = [[0,0],[2,1],[3,3]]
     return answer
+}
+
+export function possibleMoves(start) { 
+    // const evenNumbersArrow = numbers.filter(number => number % 2 === 0);
+    const moves = [[2, 1], [2, -1], [1, 2], [1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]];
+
+    // filter out any moves where start[0] + move[0] is less than 0 or greater than 7
+    // filter out any moves where start[1] + move[1] is less than 0 or greater than 7
+    const posMoves = moves.filter(move => move[0] + start[0] > 0 && move[0] + start[0] <= 7 && 
+                                        move[1] + start[1] > 0 && move[1] + start[1] <= 7 );
+
+    return posMoves;
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-import { knightMoves } from "./index.js";
+import { knightMoves, possibleMoves } from "./index.js";
 
-test('Knight move', () => {
+test('Knight solution', () => {
   expect(knightMoves([0,0],[3,3])).toEqual([[0,0],[2,1],[3,3]]);
 });
+
+test('Possible moves', () => {
+    expect(possibleMoves([0,0])).toEqual([[2, 1], [1, 2]])
+})
