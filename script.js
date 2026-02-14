37c99542124b271de95afe8245854f8a5521d997
rgremaud <rgremau@gmail.com>
2026-02-13 21:52:15 -0500

    Add mostly working script

--- script.js
+++ script.js
@@ -1,109 +1,67 @@
-/*
-New process:
-Start: 0, 0
-End: 5, 5
-
-possible moves: [2, 1] [2, -1] [1, 2] [1, -2] [-2, 1] [-2, -1] [-1, 2] [-1, -2]
-visited: [0, 0] [2, 1] [1, 2] || [4, 2] [4, 0] [3, 3] [0, 2] [1, 3] 
-
-1st move: [2, 1] [1, 2]
-2nd moves: [4, 2] [4, 0] [3, 3] [0, 2] [1, 3] || [3, 1] [2, 4] []
-
-check that position is valid on the board
-
-set up a queue of nodes to visit
-and a list of visited arrays
-
-while queue is not empty
-pop out the first coordinate and explore all 8 possible knight moves
-if move is a valid move and cell has not been visited yet then check if you are at target cell
-otherwise mark the cell as visited and push it to the queue for further exploration
-
-*/
-
 class Coordinate {
-  constructor(x, y) {
+  constructor(x, y, moves, previous=null) {
     this.x = x;
     this.y = y;
+    this.moves = moves;
+    this.previous = previous;
   }
 }
-
-export function knightMovesNew(start, end) {
-  // define possible moves
-
-  const movesX = [2, 2, 1, 1, -1, -1, -2, -2];
-  const movesY = [1, -1, 2, -1, 2, -2, 1, -1];
-  // define queue
-
-  const queue = [];
-
-
-}
-
 
 export function knightMoves(start, end) {
-  // set initial values
-  const traversal = [start];
-  let position = start;
+  // define possible moves
+  const moveX = [2, 2, 1, 1, -1, -1, -2, -2];
+  const moveY = [1, -1, 2, -1, 2, -2, 1, -1];
 
-  let posMoves = possibleMoves(position);
-  let queue = buildQueue(position, posMoves);
-
-  let nextPos = closestPoint(end, queue);
-
-  traversal.push(nextPos);
-
-  // try loop here?
-  while (position[0] !== end[0] && position[1] !== end[1]) {
-    position = nextPos;
-    posMoves = possibleMoves(position);
-    queue = buildQueue(position, posMoves);
-    nextPos = closestPoint(end, queue)
-    traversal.push(nextPos);
-
-    position = nextPos;
-  }
-
-  return traversal;
-}
-
-export function possibleMoves(start) {
-  // const evenNumbersArrow = numbers.filter(number => number % 2 === 0);
-  const moves = [
-    [2, 1],
-    [2, -1],
-    [1, 2],
-    [1, -2],
-    [-2, 1],
-    [-2, -1],
-    [-1, 2],
-    [-1, -2],
-  ];
-
-  // filter out any moves where start[0] + move[0] is less than 0 or greater than 7
-  // filter out any moves where start[1] + move[1] is less than 0 or greater than 7
-  const posMoves = moves.filter(
-    (move) =>
-      // check x axis is above 0 and less than or equal to 7
-      move[0] + start[0] >= 0 &&
-      move[0] + start[0] <= 7 &&
-      // check y axis is above 0 and less than or equal 7
-      move[1] + start[1] >= 0 &&
-      move[1] + start[1] <= 7,
-  );
-
-  return posMoves;
-}
-
-export function buildQueue(currentPos, movesArray) {
+  // define queue
   const queue = [];
-  movesArray.forEach((move) => {
-    const coordinate = [move[0] + currentPos[0], move[1] + currentPos[1]]
-    queue.push(coordinate);
-  });
 
-  return queue;
+  // add starting position to queue
+  queue.push(new Coordinate(start[0], start[1], 0))
+
+  // set temporary variables for traversal
+  let temp;
+  let x;
+  let y;
+
+  // set up visited array and update knight start position
+  const visited = Array.from({ length: 8 }, () => Array(8).fill(false));
+  visited[start[0]][start[1]] = true;
+
+  // loop locations until queue is empty
+  while ( queue.length > 0 ) { 
+    // pop first item from queue
+    temp = queue.shift();
+
+    // check if current coordinate is target
+    if (temp.x === end[0] && temp.y === end[1]) {
+      // build a list of all the previous nodes
+      // const path = [];
+      // while (temp.previous !== null) {
+      //   path.push([temp.previous.x, temp.previous.y]);
+      // }
+      return temp;
+    }
+
+    // explore all reachable locations
+    for ( let i = 0; i <= 7; i++) {
+      x = temp.x + moveX[i];
+      y = temp.y + moveY[i];
+
+      // test if coordinate is on board and not visited
+      if (validMove(x, y) && !visited[x][y]) {
+        visited[x][y] = true;
+        queue.push( new Coordinate(x, y, temp.moves + 1, temp) )
+      }
+    }
+  }
+  return "Something went wrong";
 }
+
+export function validMove(x, y) {
+  return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+}
+
+console.log(knightMoves([0, 0], [3, 3]));
 
 
 
--- script.test.js
+++ script.test.js
@@ -1,17 +1,15 @@
-import { knightMoves, possibleMoves, buildQueue, closestPoint, distance, knightMovesNew } from "./index.js";
+import { knightMoves, validMove } from "./script.js";
 
 test('Knight solution', () => {
-    expect(knightMoves([0, 0], [3, 3])).toStrictEqual([[0, 0], [2, 1], [3, 3]]);
-    expect(knightMoves([0, 0], [7, 7])).toStrictEqual([[0, 0], [2, 1], [4, 2], [6, 3], [7, 5], [5, 6], [7, 7]]);
+    // expect(knightMoves([0, 0], [3, 3])).toStrictEqual([[0, 0], [2, 1], [3, 3]]);
+    // expect(knightMoves([0, 0], [7, 7])).toStrictEqual([[0, 0], [2, 1], [4, 2], [6, 3], [7, 5], [5, 6], [7, 7]]);
+    expect(knightMoves([0, 0], [3, 3])).toBe(2);
 });
 
-// rework tests so you're current pos is [2,1] 
+test('Valid move', () => {
+    expect(validMove(-1, 1)).toBe(false);
+    expect(validMove(0, 0)).toBe(true);
+    expect(validMove(8, 1)).toBe(false);
+    expect(validMove(7, 7)).toBe(true);
+});
 
-test('Possible moves', () => {
-    expect(possibleMoves([2, 1])).toEqual([[2, 1], [2, -1], [1, 2], [-2, 1], [-2, -1], [-1, 2]])
-})
-
-test('Build queue', () => {
-    expect(buildQueue([0, 0], [[2, 1], [1, 2]])).toEqual([[2, 1], [1, 2]]);
-    expect(buildQueue([2, 1], [[2, 1], [2, -1], [1, 2], [-2, 1], [-2, -1], [-1, 2]])).toEqual([[4, 2], [4, 0], [3, 3], [0, 2], [0, 0], [1, 3]]);
-})
